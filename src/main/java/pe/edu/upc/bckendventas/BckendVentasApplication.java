f80c0efbfd83cd136b9ea4a6e4aa6601959d1023
Veronica Ruth Mendoza Hancco <U202120604@upc.edu.pe>
2024-12-10 21:43:15 -0500

    Creación del Proyecto

---
+++ src/main/java/pe/edu/upc/bckendventas/BckendVentasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.bckendventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BckendVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BckendVentasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/bckendventas/BckendVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.bckendventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BckendVentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
